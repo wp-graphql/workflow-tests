373a30828b0df867fa2ed43e56eb4bb557484dc3
jasonbahl <jasonbahl@users.noreply.github.com>
2025-11-15 00:19:18 +0000

    release: prepare v6.0.16

--- automation-tests.php
+++ automation-tests.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Automation Tests
  * Description: A plugin to test automation workflows.
- * Version: 6.0.15
+ * Version: 6.0.16
  * Author: Jason Bahl
  * Tested up to: 6.2
  */
--- constants.php
+++ constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('AUTOMATION_TESTS_VERSION', '6.0.15');
+define('AUTOMATION_TESTS_VERSION', '6.0.16');
 define('AUTOMATION_TESTS_PLUGIN_NAME', 'automation-tests');
 
 /**
